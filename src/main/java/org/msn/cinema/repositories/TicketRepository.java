c346a288cbe9b8a143f2b30cc2cf863eaeb6114a
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2023-07-01 16:10:16 +0000

    cinema-backend

---
+++ src/main/java/org/msn/cinema/repositories/TicketRepository.java
@@ -0,0 +1,12 @@
+package org.msn.cinema.repositories;
+
+import org.msn.cinema.entities.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin("*")
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+}
---
+++ src/main/java/org/msn/cinema/service/CinemaInitServiceImpl.java
@@ -0,0 +1,181 @@
+package org.msn.cinema.service;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import javax.transaction.Transactional;
+
+import org.msn.cinema.entities.Categorie;
+import org.msn.cinema.entities.Cinema;
+import org.msn.cinema.entities.Film;
+import org.msn.cinema.entities.Place;
+import org.msn.cinema.entities.Projection;
+import org.msn.cinema.entities.Salle;
+import org.msn.cinema.entities.Seance;
+import org.msn.cinema.entities.Ticket;
+import org.msn.cinema.entities.Ville;
+import org.msn.cinema.repositories.CategorieRepository;
+import org.msn.cinema.repositories.CinemaRepository;
+import org.msn.cinema.repositories.FilmRepository;
+import org.msn.cinema.repositories.PlaceRepository;
+import org.msn.cinema.repositories.ProjectionRepository;
+import org.msn.cinema.repositories.SalleRepository;
+import org.msn.cinema.repositories.SeanceRepository;
+import org.msn.cinema.repositories.TicketRepository;
+import org.msn.cinema.repositories.VilleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CinemaInitServiceImpl implements ICinemaInitService {
+	
+	@Autowired
+	private VilleRepository villeRepository;
+	@Autowired
+	private CinemaRepository cinemaRepository;
+	@Autowired
+	private SalleRepository salleRepository;
+	@Autowired
+	private PlaceRepository placeRepository;
+	@Autowired
+	private SeanceRepository seanceRepository;
+	@Autowired
+	private FilmRepository filmRepository;
+	@Autowired
+	private ProjectionRepository projectionRepository;
+	@Autowired
+	private CategorieRepository categorieRepository;
+	@Autowired
+	private TicketRepository ticketRepository;
+	
+	@Override
+	public void initVilles() {
+		Stream.of("Casablanca","Marrakech","Rabat","Tanger").forEach(nameVille->{
+			Ville ville = new Ville();
+			ville.setName(nameVille);
+			villeRepository.save(ville);
+		});	
+	}
+
+	@Override
+	public void initCinemas() {
+		villeRepository.findAll().forEach(v->{
+			Stream.of("MegaRama","IMAX","FOUNOUN","CHAHRAZAD","DAOULIZ")
+			 .forEach(nameCinema->{
+				 Cinema cinema = new Cinema();
+				 cinema.setName(nameCinema);
+				 cinema.setNombreSalles(3+(int)(Math.random()*7));
+				 cinema.setVille(v);
+				 cinemaRepository.save(cinema);
+			 });
+		});	
+	}
+
+	@Override
+	public void initSalles() {
+		cinemaRepository.findAll().forEach(cinema->{
+			for(int i=0;i<cinema.getNombreSalles();i++) {
+				Salle salle = new Salle();
+				salle.setName("Salle "+(i+1));
+				salle.setCinema(cinema);
+				salle.setNombrePlaces(15+(int)(Math.random()*20));
+				salleRepository.save(salle);
+			}
+		});	
+	}
+
+	@Override
+	public void initPlaces() {
+		salleRepository.findAll().forEach(salle->{
+			for(int i=0;i<salle.getNombrePlaces();i++) {
+				Place place = new Place();
+				place.setNumero(i+1);
+				place.setSalle(salle);
+				placeRepository.save(place);
+			}
+		});
+	}
+
+	@Override
+	public void initSeances() {
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		Stream.of("12:00","15:00","17:00","19:00","21:00").forEach(s->{
+			Seance seance = new Seance();
+			try {
+				seance.setHeureDebut(dateFormat.parse(s));
+				seanceRepository.save(seance);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		});
+		
+	}
+
+	@Override
+	public void initCategories() {
+		Stream.of("Histoire","Actions","Fiction","Drama").forEach(cat->{
+			Categorie categorie = new Categorie();
+			categorie.setName(cat);
+			categorieRepository.save(categorie);
+		});	
+	}
+
+	@Override
+	public void initFilms() {
+		double[] durees = new double[] {1,15,2,2.5,3};
+		List<Categorie> categorie = categorieRepository.findAll();
+		Stream.of("Game Of Thrones","Stalk","Social Network","Spider Man","Casa de Papel","Seigneur des anneaux")
+		.forEach(titreFilm->{
+			Film film = new Film();
+			film.setTitre(titreFilm);
+			film.setDuree(durees[new Random().nextInt(durees.length)]);
+			film.setPhoto(titreFilm.replaceAll(" ","")+".jpg");
+			film.setCategorie(categorie.get(new Random().nextInt(categorie.size())));
+			filmRepository.save(film);
+		});		
+	}
+
+	@Override
+	public void initProjections() {
+		double[] prices = new double[] {30,50,60,70,90,100};
+		List<Film> films = filmRepository.findAll();
+		villeRepository.findAll().forEach(ville->{
+			ville.getCinemas().forEach(cinema->{
+				cinema.getSalles().forEach(salle->{
+					int index = new Random().nextInt(films.size());
+					Film film = films.get(index);
+						seanceRepository.findAll().forEach(seance->{
+							Projection projection = new Projection();
+							projection.setDateProjection(new Date());
+							projection.setFilm(film);
+							projection.setPrix(prices[new Random().nextInt(prices.length)]);
+							projection.setSalle(salle);
+							projection.setSeance(seance);
+							projectionRepository.save(projection);
+					});			
+				});
+			});
+		});
+	}
+
+	@Override
+	public void initTickets() {
+		projectionRepository.findAll().forEach(p->{
+			p.getSalle().getPlaces().forEach(place->{
+				Ticket ticket = new Ticket();
+				ticket.setPlace(place);
+				ticket.setPrix(p.getPrix());
+				ticket.setProjection(p);
+				ticket.setReserve(false);
+				ticketRepository.save(ticket);
+			});
+		});
+	}
+
+}
